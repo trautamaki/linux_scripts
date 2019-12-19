1baa5348be6b03579d7473c66b9dbb7dd6305bd0
rautamak <13891897+rautamak@users.noreply.github.com>
2019-12-19 10:48:59 +0100

    Use $(nproc --all)

--- makerom.sh
+++ makerom.sh
@@ -3,7 +3,6 @@
 . build/envsetup.sh
 
 export USE_CCACHE=1
-prebuilts/misc/linux-x86/ccache/ccache -M 80G
 export CCACHE_EXEC=/usr/bin/ccache
 
 lunch aosp_cheeseburger-userdebug
@@ -13,8 +12,8 @@ make system-api-stubs-docs
 make test-api-stubs-docs
 make kronic
 
-make otapackage -j6
+make otapackage -j$(nproc --all)
 
 lunch aosp_dumpling-userdebug
 
-make otapackage -j6
+make otapackage -j$(nproc --all)
