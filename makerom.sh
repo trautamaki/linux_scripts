c10d9db1eb5294b372fbe8f69a50f7e643bd71a9
rautamak <13891897+rautamak@users.noreply.github.com>
2019-12-19 09:38:52 +0100

    makerom: use ccache

--- makerom.sh
+++ makerom.sh
@@ -2,6 +2,10 @@
 
 . build/envsetup.sh
 
+export USE_CCACHE=1
+prebuilts/misc/linux-x86/ccache/ccache -M 80G
+export CCACHE_EXEC=/usr/bin/ccache
+
 lunch aosp_cheeseburger-userdebug
 make api-stubs-docs
 make hiddenapi-lists-docs
@@ -9,4 +13,8 @@ make system-api-stubs-docs
 make test-api-stubs-docs
 make kronic
 
-make otapackage -j8
+make otapackage -j6
+
+lunch aosp_dumpling-userdebug
+
+make otapackage -j6
