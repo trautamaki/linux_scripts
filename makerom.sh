c244136a305d578166eb822a438e34795201e8cd
rautamak <13891897+rautamak@users.noreply.github.com>
2020-01-07 18:06:19 +0100

    add check to make for dumpling

--- makerom.sh
+++ makerom.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+unset ALL
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -a|--all)
+    ALL="1"
+    shift
+    shift
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}"
+
+if [[ $ALL == "1" ]]; then
+    echo -e "\e[33m=======================\e[0m"
+    echo -e "\e[33m   Making all targets  \e[0m"
+    echo -e "\e[33m=======================\e[0m"
+else
+    echo -e "\e[33m=======================\e[0m"
+    echo -e "\e[33mMaking only cheeseburger\e[0m"
+    echo -e "\e[33m=======================\e[0m"
+fi
+
 . build/envsetup.sh
 
 export USE_CCACHE=1
@@ -13,6 +40,7 @@ make test-api-stubs-docs
 
 make otapackage -j$(nproc --all)
 
-#lunch aosp_dumpling-userdebug
-
-#make otapackage -j$(nproc --all)
+if [[ $ALL == "1" ]]; then
+    lunch aosp_dumpling-userdebug
+    make otapackage -j$(nproc --all)
+fi
