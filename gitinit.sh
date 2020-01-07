4138c59b5fb2fadb4d80abed64bda431b1484a6d
rautamak <13891897+rautamak@users.noreply.github.com>
2020-01-07 18:03:45 +0100

    set autoindent for nano

--- gitinit.sh
+++ gitinit.sh
@@ -1,2 +1,7 @@
 git config --global alias.st status
 git config --global alias.chp cherry-pick
+
+
+echo "set tabsize 4" >> ~/.nanorc
+echo "set tabstospaces" >> ~/.nanorc
+echo "set autoindent" >> ~/.nanorc
