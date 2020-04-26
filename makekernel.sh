7854e2f14cc3ac50ba78bb422e199f6c05258c59
rautamak <timi.rautamaki@gmail.com>
2020-04-26 13:07:24 +0300

    makekernel: check for existing gcc/clang path

--- makekernel.sh
+++ makekernel.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-export PATH="/root/caf10/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:${PATH}"
-export PATH="/root/caf10/prebuilts/clang/host/linux-x86/clang-6305083/bin:${PATH}"
+default_abi="~/caf10/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin"
+default_clang="~/caf10/prebuilts/clang/host/linux-x86/clang-r353983c/bin"
+
+if [[ ${PATH} == *"clang-"* ]]; then
+    echo "Clang found in PATH"
+else
+    echo "Clang not found in PATH"
+    echo "Using default Clang path"
+    export PATH="${default_clang}:${PATH}"
+fi
+
+if [[ ${PATH} == *"aarch64-linux-android-4.9"* ]]; then
+    echo "GCC found in PATH"
+else
+    echo "GCC not found in PATH"
+    echo "Using default GCC path"
+    export PATH="${default_abi}:${PATH}"
+fi
+
 export ARCH=arm64
 export SUBARCH=arm64
 
@@ -63,7 +80,7 @@ if [[ $CLANG == "" ]] || [[ $CLANG == "y" ]] || [[ $CLANG == "clang" ]]; then
     echo -e "\e[33m  Building with Clang  \e[0m"
     echo -e "\e[33m=======================\e[0m"
     make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android-
-    
+
     if [ $? -eq 0 ];then
         echo -e "\e[32m=======================\e[0m"
         echo -e "\e[32m   Compile successful  \e[0m"
@@ -78,7 +95,7 @@ else
     echo -e "\e[33m=======================\e[0m"
     echo -e "\e[33m   Building with GCC   \e[0m"
     echo -e "\e[33m=======================\e[0m"
-    make O=out -j$(nproc --all) CROSS_COMPILE=/root/caf10/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+    make O=out -j$(nproc --all) CROSS_COMPILE=aarch64-linux-android-
 
     if [ $? -eq 0 ];then
         echo -e "\e[32m=======================\e[0m"
