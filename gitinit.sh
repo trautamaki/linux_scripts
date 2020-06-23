ff50693493cfd23515181b12b23ca5af55adc92a
trautamaki <timi.rautamaki@gmail.com>
2020-06-23 08:11:28 +0200

    Add git alias to cherry-pick from Github link

--- gitinit.sh
+++ gitinit.sh
@@ -1,6 +1,6 @@
 git config --global alias.st status
 git config --global alias.chp cherry-pick
-
+git config --global alias.hubchp '! git fetch $(echo $1 | cut -d / -f-5) && git chp $(echo $1 | cut -d / -f 7-) && :'
 
 echo "set tabsize 4" >> ~/.nanorc
 echo "set tabstospaces" >> ~/.nanorc
--- unzip_rom.sh
+++ unzip_rom.sh
@@ -45,7 +45,7 @@ unzip -o ${ZIP} system.transfer.list system.new.dat* vendor.transfer.list vendor
     -d ${DIR} || { echo 'Unzipping failed' ; exit 1; }
 
 # Handle brotli compressed files
-if [[ ! -f ${DIR}/*.br ]]; then
+if [[ -f ${DIR}/*.br ]]; then
     rm ${DIR}/system.new.dat
     rm ${DIR}/vendor.new.dat
     echo "Decompressing brotli files"
