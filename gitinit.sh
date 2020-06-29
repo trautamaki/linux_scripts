1fea919b4a116f9cd9f3353cd56573d3cf84472a
rautamak <timi.rautamaki@gmail.com>
2020-06-29 09:09:13 +0300

    Comment gitinit & allow other than default branch for githubchp

--- gitinit.sh
+++ gitinit.sh
@@ -1,7 +1,15 @@
 git config --global alias.st status
-git config --global alias.chp cherry-pick
-git config --global alias.hubchp '! git fetch $(echo $1 | cut -d / -f-5) && git chp $(echo $1 | cut -d / -f 7-) && :'
 
+# Usage: git chp <hash>
+git config --global alias.chp cherry-pick
+
+# Usage: git hubchp <github commit link> <branch>
+# Branch can be left empty, if commit is on default (git fetch) branch
+# Example:
+#   git hubchp https://github.com/trautamaki/linux_scripts/commit/c58a383a933857ed830c41c2c9e3a85e5382ed07 aplha
+git config --global alias.hubchp '! git fetch $(echo $1 | cut -d / -f-5) $2 && git chp $(echo $1 | cut -d / -f 7-) && :'
+
+# nano config
 echo "set tabsize 4" >> ~/.nanorc
 echo "set tabstospaces" >> ~/.nanorc
 #echo "set autoindent" >> ~/.nanorc
