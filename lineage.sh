d9dbff0091df0895904682454b9f1d70f98f8020
Timi Rautamäki <timi.rautamaki@gmail.com>
2020-08-18 11:06:45 +0200

    Add simple make script for LineageOS

---
+++ lineage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+. build/envsetup.sh
+
+export USE_CCACHE=true
+export CCACHE_EXEC=/usr/bin/ccache
+
+lunch lineage_cheeseburger-userdebug
+make api-stubs-docs
+make hiddenapi-lists-docs
+make system-api-stubs-docs
+make test-api-stubs-docs
+
+make otapackage -j$(nproc --all)
